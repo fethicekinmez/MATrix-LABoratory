152627ca3a12249c6e691dc60bff56818d5329a7
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-06-02 19:26:43 +0300

    Update PD-controller

--- 13- Proportional Derivative Control/PD_Controller.m
+++ 13- Proportional Derivative Control/PD_Controller.m
@@ -8,8 +8,15 @@ step(Y)
 hold on;
 
 %% Kp Kd added
-Kp = 50; % Proportional gain
+Kp = 500; % Proportional gain
 Kd = 10; % Derivative gain
 C = tf([Kd Kp], 1) % PD controller
 Y_pd = feedback(C*G, 1); % Closed-loop system with PD controller
 step(Y_pd); % Step response of the PID controlled system
+
+Kp = 500; % Proportional gain
+Kd = 20; % Derivative gain
+C = tf([Kd Kp], 1) % PD controller
+Y_pd = feedback(C*G, 1); % Closed-loop system with PD controller
+step(Y_pd); % Step response of the PID controlled system
+legend('No Kp-Kd', 'Kp=500 Kd=10', 'Kp=500 Kd=20');
